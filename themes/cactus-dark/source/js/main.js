c5d9b5fdb9084bebfc60553054e1c56fa5898358
Ferdinand Mütsch <ferdinand@muetsch.io>
2021-06-26 11:36:37 +0200

    fix menu toggle

--- themes/cactus-dark/source/js/main.js
+++ themes/cactus-dark/source/js/main.js
@@ -10,7 +10,11 @@ document.addEventListener('DOMContentLoaded', function () {
   /**
    * Shows the responsive navigation menu on mobile.
    */
-  var menuIcons = document.getElementById('header').getElementsByClassName('icon')
+  var header = [
+    document.getElementById('header'),
+    document.getElementById('header-post')
+  ].filter(e => !!e)[0]
+  var menuIcons = header.getElementsByClassName('icon')
   if (menuIcons.length) menuIcons[0].addEventListener('click', function (e) {
     menuIcons[0].parentElement.classList.toggle('responsive')
   })
@@ -19,18 +23,20 @@ document.addEventListener('DOMContentLoaded', function () {
     * Display the menu if the menu icon is clicked.
     */
   if ([...document.querySelectorAll('.post')].length) {
-    var menu = document.getElementById('menu')
-    [document.getElementById('menu-icon'), document.getElementById('menu-icon-tablet')].forEach(function (menuIcon) {
-      menuIcon.addEventListener('click', function (e) {
-        if (!menu.style.visibility || menu.style.visibility === 'hidden') {
-          menu.style.visibility = 'visible'
-          menuIcon.classList.add('active')
-        } else {
-          menu.style.visibility = 'hidden'
-          menuIcon.classList.remove('active')
-        }
-        e.preventDefault()
+    var menu = document.getElementById('menu');
+    [document.getElementById('menu-icon'), document.getElementById('menu-icon-tablet')]
+      .filter(e => !!e)
+      .forEach(function (menuIcon) {
+        menuIcon.addEventListener('click', function (e) {
+          if (!menu.style.visibility || menu.style.visibility === 'hidden') {
+            menu.style.visibility = 'visible'
+            menuIcon.classList.add('active')
+          } else {
+            menu.style.visibility = 'hidden'
+            menuIcon.classList.remove('active')
+          }
+          e.preventDefault()
+        })
       })
-    })
   }
 });
