f03949f5c7341db9b03f33f5ed72e52ba3c68897
Ferdinand Mütsch <ferdinand@muetsch.io>
2025-07-31 10:57:14 +0200

    minor fixes

--- themes/cactus-dark/source/js/cusdis.js
+++ themes/cactus-dark/source/js/cusdis.js
@@ -1,22 +1,24 @@
 // https://github.com/djyde/cusdis/issues/283#issuecomment-2543119916
 window.addEventListener('load', function () {
-    const iframe = document.querySelector('#cusdis_thread iframe')
-    
-    if (iframe) {
-        const iframeBody = iframe.contentWindow.document.body
+    setTimeout(() => {
+        const iframe = document.querySelector('#cusdis_thread iframe')
 
-        // Fix height
-        let observer = new MutationObserver(() => {
-            let scrollHeight = iframe.contentWindow.document.body.scrollHeight
-            iframe.style.height = scrollHeight + 24 + 'px'
-        })
-        observer.observe(iframe.contentWindow.document.body, { childList: true, subtree: true })
+        if (iframe) {
+            const iframeBody = iframe.contentWindow.document.body
 
-        // Fix background color, font, credits and textbox label
-        iframeBody.style.backgroundColor = '#1d1f21'
-        iframeBody.style.fontFamily = '"Roboto Mono", monospace'
-        iframeBody.querySelector("[for='reply_content']").innerText = 'Your comment'
-        iframeBody.querySelector(".text-center").previousElementSibling.remove()
-        iframeBody.querySelector(".text-center").remove()
-    }
+            // Fix height
+            let observer = new MutationObserver(() => {
+                let scrollHeight = iframe.contentWindow.document.body.scrollHeight
+                iframe.style.height = scrollHeight + 32 + 'px'
+            })
+            observer.observe(iframeBody, { childList: true, subtree: true })
+
+            // Fix background color, font, credits and textbox label
+            iframeBody.style.backgroundColor = '#1d1f21'
+            iframeBody.style.fontFamily = '"Roboto Mono", monospace'
+            iframeBody.querySelector("[for='reply_content']").innerText = 'Your comment'
+            iframeBody.querySelector(".text-center").previousElementSibling.remove()
+            iframeBody.querySelector(".text-center").remove()
+        }
+    }, 100)
 });
